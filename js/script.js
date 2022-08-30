eaebf6df27d1250ede64c8e2d2d48b2096c06bce
banssa <ban8627@gmail.com>
2022-08-30 18:08:30 +0900

    채용섹션 작성중

--- js/script.js
+++ js/script.js
@@ -3,4 +3,18 @@ $(document).ready(function () {
 
 
 window.onload = function () {
+  new Swiper('.sw-news', {
+  spaceBetween: 50,
+  slidesPerView: "auto",
+  speed: 1000,
+  loop: true,
+  navigation: {
+    nextEl: '.news_next',
+    prevEl: '.news_prev',
+  },
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: true,
+  },
+	});
 };
