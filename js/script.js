7f0c9c735a8e82fccedf0edffe89d49e012ce571
banssa <ban8627@gmail.com>
2022-09-01 18:04:26 +0900

    js 작업중

--- js/script.js
+++ js/script.js
@@ -1,9 +1,38 @@
 $(document).ready(function () {
+  let gnb = $('.gnb');
+  let hInner = $('.header > .inner');
+  let logo = $('.logo');
+  let quickBt = $('.quick-bt');
+  let siteMap = $('.site-map');
+  let subMenu = $('.drop-menu');
+
+  gnb.mouseenter(() => {
+    logo.addClass('active');
+    hInner.addClass('active');
+    quickBt.addClass('active');
+    siteMap.addClass('active');
+    subMenu.stop().slideDown();
+  });
+  gnb.mouseleave(() => {
+    logo.removeClass('active');
+    hInner.removeClass('active');
+    quickBt.removeClass('active');
+    siteMap.removeClass('active');
+    subMenu.stop().slideUp();
+  })
+
+  // family-site
+  let fSiteBt = $('.family-site > a');
+  let fSiteList = $('.family-site > ul');
+  fSiteBt.click((event) => {
+    event.preventDefault();
+    fSiteList.stop().slideToggle();
+  })
 });
 
 
 window.onload = function () {
-  new Swiper('.sw-news', {
+  let swNews = new Swiper('.sw-news', {
   spaceBetween: 50,
   slidesPerView: "auto",
   speed: 1000,
@@ -16,5 +45,15 @@ window.onload = function () {
     delay: 5000,
     disableOnInteraction: true,
   },
-	});
+  });
+  
+  let swContri = new Swiper('.sw-contri', {
+    slidesPerView: 1,
+    loop: true,
+    speed: 1000,
+    autoplay: {
+      delay: 1000,
+      disableOnInteraction:false
+    }
+  })
 };
